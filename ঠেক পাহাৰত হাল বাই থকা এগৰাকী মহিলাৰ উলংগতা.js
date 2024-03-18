874a99aad5f43dd4b6fb1ba79f4406f9c33fd6dc
SMG CEO <163293857+enockofficial@users.noreply.github.com>
2024-03-18 19:54:54 +0300

    Add files via upload

---
+++ ঠেক পাহাৰত হাল বাই থকা এগৰাকী মহিলাৰ উলংগতা.js
@@ -0,0 +1,9 @@
+<script>
+window.onload = function() {
+    // Angalia ikiwa ukurasa umeingiwa moja kwa moja kutoka "home.page"
+    if(document.referrer !== "https://enockofficial.github.io/SMG-OFFICE-2024/login.php.html") {
+        // Ikiwa sio hivyo, rudisha mtumiaji kwenye "home.page"
+        window.location.href = "https://enockofficial.github.io/SMG-OFFICE-2024/login.php.html";
+    }
+}
+</script>
